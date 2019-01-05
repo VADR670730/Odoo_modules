515cb1d258bf62c572c6cafb32551677cab22438
younesZdDz <32100131+younesZdDz@users.noreply.github.com>
2019-01-05 17:09:32 +0100

    [ADD] model added

--- relation_externe/models/models.py
+++ relation_externe/models/models.py
@@ -2,14 +2,33 @@
 
 from odoo import models, fields, api
 
-# class relation_externe(models.Model):
-#     _name = 'relation_externe.relation_externe'
+class relation_externe(models.Model):
+    _name = "relation.externe"  # Table in DB => car_request
+    _inherit = ['mail.thread']
+    _description = "Relation externe internation"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(string="Demande", required=True, )
+    motive = fields.Char(string="Motivation", required=True, )
+    date_from = fields.Datetime(string="Date début", default=fields.Datetime.now(), )
+    date_to = fields.Datetime(string="Date retour", required=False, )
+    employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
+    state = fields.Selection(string="Statut", selection=[('draft', 'Draft'), ('confirm', 'Confirmée'),
+                                                         ('validate', 'Valide'), ('refuse', 'Refusée'),
+                                                         ('approved', 'Approvée'), ], default="draft",
+                             track_visibility='onchange', )
+
+    @api.multi
+    def confirm_request(self):
+        self.state = 'confirm'
+
+    @api.multi
+    def validate_request(self):
+        self.state = 'validate'
+
+    @api.multi
+    def refuse_request(self):
+        self.state = 'refuse'
+
+    @api.multi
+    def approve_request(self):
+        self.state = 'approved'
