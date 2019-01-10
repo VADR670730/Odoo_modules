4461f546540cacf5fd81ebe2a0b9b882d078e043
younesZdDz <32100131+younesZdDz@users.noreply.github.com>
2019-01-11 00:16:36 +0100

    [ADD] bon_commande model added

--- relation_externe/__manifest__.py
+++ relation_externe/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "relation_externe",
+    'name': "Post_grad",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        """,
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "ESI",
+    'website': "http://www.ESI.dz",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human.Ressources',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,8 +23,11 @@
 
     # always loaded
     'data': [
+        'security/user_groups.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
+        'views/view_demande_charge.xml',
+        'views/view_bon_commande.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- relation_externe/models/models.py
+++ relation_externe/models/models.py
@@ -2,33 +2,49 @@
 
 from odoo import models, fields, api
 
-class relation_externe(models.Model):
-    _name = "relation.externe"  # Table in DB => car_request
-    _inherit = ['mail.thread']
-    _description = "Relation externe internation"
+class demande_charge(models.Model):
+    _name = "demande.charge"  #Nom de latable dan la base de donnée  => demande_charge
+    _inherit = ['mail.thread']    #Hériter le module mail pour l'envoie des email lors du changement d'état de la demande (refus...)
+    _description = "Faire une demande de prise en charge dans le cadre d'un stage, séminaire, ou conférence pou les post-graduation"
 
-    name = fields.Char(string="Demande", required=True, )
-    motive = fields.Char(string="Motivation", required=True, )
-    date_from = fields.Datetime(string="Date début", default=fields.Datetime.now(), )
-    date_to = fields.Datetime(string="Date retour", required=False, )
-    employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
-    state = fields.Selection(string="Statut", selection=[('draft', 'Draft'), ('confirm', 'Confirmée'),
+    name = fields.Char(string="Titre de la demande", required=True, )   #Titre de la demane
+    motive = fields.Char(string="Motivation", required=True, )   #La raison de vouloire participer
+    date_from = fields.Datetime(string="Date début", default=fields.Datetime.now(), )  #date de départ
+    date_to = fields.Datetime(string="Date retour", required=False, )  #date de retour
+    employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )  #l'id de l'employé
+    state = fields.Selection(string="Statut", selection=[('draft', 'Draft'),('incomplet','Incomplet'), ('confirm', 'Confirmée'),
                                                          ('validate', 'Valide'), ('refuse', 'Refusée'),
                                                          ('approved', 'Approvée'), ], default="draft",
-                             track_visibility='onchange', )
+                             track_visibility='onchange', ) #l'état de la demande
 
     @api.multi
-    def confirm_request(self):
+    def confirm_request(self):   #action de confirmation
         self.state = 'confirm'
 
     @api.multi
-    def validate_request(self):
+    def validate_request(self):   #action de validation demande
         self.state = 'validate'
 
     @api.multi
-    def refuse_request(self):
+    def refuse_request(self):   #action  de refus
         self.state = 'refuse'
 
     @api.multi
-    def approve_request(self):
+    def approve_request(self):   #action d'approuver
         self.state = 'approved'
+
+    @api.multi
+    def incomplet_request(self):  # action d'approuver
+        self.state = 'incomplet'
+
+
+class bon_commande(models.Model):
+    _name = "bon.commande"  #Nom de latable dan la base de donnée  => bon_commande
+    _inherit = ['mail.thread']
+    _description = "Creer des bons de commande pour les doctorants ayant été approuvé"
+
+    name = fields.Char(string="Numéro du bon", required=True, )
+    date_from = fields.Datetime(string="Date début", default=fields.Datetime.now(), )
+    date_to = fields.Datetime(string="Date retour", required=False, )
+    employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
+    value=fields.Float(string="Montant", default=100,required=True)  #le montant de la comande
